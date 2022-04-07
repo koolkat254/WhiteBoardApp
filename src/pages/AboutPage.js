8dbeb2c54426e6bf2c608a5665e195e90494a368
Noodle873 <102526894+Noodle873@users.noreply.github.com>
2022-04-07 11:51:53 -0500

    css cleaned, notes added

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,6 +8,7 @@ const AboutPage = () => {
             <h1 className={styles.labeltxt}>About</h1>
         </div>
         <div>
+          {/* About Text */}
           <p className={styles.prg}>Welcome to our Whiteboard App! We were originally a school project that our teacher had assigned to us.</p>
           <p className={styles.prg}>This Whiteboard allows people to take photos of their art and drawings on a whiteboard. You are them able
             to send those photos on a digital whiteboard. Then the final product would be a digital whiteboard containing all of your drawings
@@ -15,15 +16,20 @@ const AboutPage = () => {
             users will vote on which is the best one and the winnings will be posted in the "Results" page.</p>
         </div>
       </div>
-      <div className={styles.creators}>
-        <h1>Creators</h1>
+      <div className={styles.box}>
+        <div className={styles.boxlabel}>
+          <h1 className={styles.labeltxt}>Creators</h1>
+        </div>
         {/* Creators Names */}
 
       </div>
-      <div className={styles.contact}>
-        <h1>Contact</h1>
+      <div className={styles.box}>
+        <div className={styles.boxlabel}>
+          <h1 className={styles.labeltxt}>Contact</h1>
+        </div>
+        {/* Contact Info */}
+        
       </div>
-    
     </div>
   )
 }
