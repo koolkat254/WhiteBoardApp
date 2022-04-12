50b2d28c873fb451a5055a9ac888d8f20d351e11
Hmerduman <102526948+Hmerduman@users.noreply.github.com>
2022-04-12 07:35:33 -0500

    About and Homepage Clean

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,13 +4,18 @@ const AboutPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.aboutbox}>
-      <h1></h1>
+      <h1>About</h1>
+      <p>Welcome to our Whiteboard App! We were originally a school project that our teacher had assigned to us.</p>
+      <p>This Whiteboard allows people to take photos of their art and drawings on a whiteboard. You are them able
+        to send those photos on a digital whiteboard. Then the final product would be a digital whiteboard containing all of your drawings
+        you had taken a photo of. You can also have your whiteboard enter a competition with another whiteboard made by another user. Other
+        users will vote on which is the best one and the winnings will be posted in the "Results" page.</p>
       </div>
       <div className={styles.creatorsbox}>
-        <h1></h1>
+        <h1>Creators</h1>
       </div>
       <div className={styles.contactsbox}>
-        <h1></h1>
+        <h1>Contact</h1>
       </div>
     </div>
   )
